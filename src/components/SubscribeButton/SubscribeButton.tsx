b34e7cd3faea69f08fbc74debc406548f2832a9c
LucasHenrique98 <henrique.lucas30@gmail.com>
2021-04-10 17:38:30 -0300

    Consume and show informations about the product from the Stripe API

--- src/components/SubscribeButton/SubscribeButton.tsx
+++ src/components/SubscribeButton/SubscribeButton.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import styles from './style.module.scss';
 
-export default function SubscribeButton() {
+interface SubscriberButtonProps {
+  priceId: string;
+}
+
+export default function SubscribeButton({ priceId }: SubscriberButtonProps) {
   return (
     <button type="button" className={styles.subscribeButton}>
       Subscribe now
