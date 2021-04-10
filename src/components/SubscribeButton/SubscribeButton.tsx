6b3ee5d5871e6e47be8c1fa39abb3d8f194b0225
LucasHenrique98 <henrique.lucas30@gmail.com>
2021-04-10 14:40:57 -0300

    Create and add the subsribe button to the home page

---
+++ src/components/SubscribeButton/SubscribeButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './style.module.scss';
+
+export default function SubscribeButton() {
+  return (
+    <button type="button" className={styles.subscribeButton}>
+      Subscribe now
+    </button>
+  );
+}
