bf2c619159efe65eaca37a47280142619e546f13
LucasHenrique98 <henrique.lucas30@gmail.com>
2021-04-11 13:03:29 -0300

    Create an authentication function to the Sign in button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import SubscribeButton from '../components/SubscribeButton/SubscribeButton';
 import styles from './home.module.scss';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { stripe } from '../services/stripe';
 
 interface HomeProps {
@@ -34,7 +34,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve(
     'price_1IekvVLG9R0FWRrBEmuZ3MaD',
     {}
@@ -50,5 +50,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
   return {
     props: { product },
+    revalidate: 60 * 60 * 24, //24 horas
   };
 };
