106d33728f1dc909d53834b64b0e46cfe8698bcd
LucasHenrique98 <henrique.lucas30@gmail.com>
2021-04-16 10:57:31 -0300

    Config the webhooks and faunadb queries

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -18,7 +18,12 @@ export const config = {
   api: { bodyParser: false },
 };
 
-const relevantEvents = new Set(['checkout.session.completed']);
+const relevantEvents = new Set([
+  'checkout.session.completed',
+  'customer.subscription.created',
+  'customer.subscription.updated',
+  'customer.subscription.deleted',
+]);
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
@@ -42,13 +47,26 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     if (relevantEvents.has(type)) {
       try {
         switch (type) {
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            );
+
+            break;
+
           case 'checkout.session.completed':
             const checkoutSessions = event.data
               .object as Stripe.Checkout.Session;
 
             await saveSubscription(
               checkoutSessions.subscription.toString(),
-              checkoutSessions.customer.toString()
+              checkoutSessions.customer.toString(),
+              true
             );
 
             break;
